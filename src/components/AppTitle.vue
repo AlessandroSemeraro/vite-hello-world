1ebd2f7c2a2fdb2cefc7abbdf5ec2b3b8cc737aa
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-12-16 19:09:19 +0100

    added title

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'MyHelloTry'
+}
+</script>
+
+<template>
+    <div>
+        <h1>"La mia prima app con Vite!"</h1>
+    </div>
+</template>
+
+<style>
+    h1{
+        color: magenta;
+        font-size: 2rem;
+    }
+</style>
